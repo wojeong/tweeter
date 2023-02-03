13f6101db1e56048a0273b0664cb55cca5c34c5d
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-02-02 23:51:46 -0500

    client.js for Dynamic Tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,71 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+  for (const tweet of tweets) {
+    const aTweet = createTweetElement(tweet);
+    $('#tweets-container').append(aTweet);
+  }
+}
+
+const createTweetElement = function(tweet) {
+  const timePassed = timeago.format(tweet.created_at);  
+  const $tweet = `
+  <article class="tweet">
+    <header>
+      <div class="tweet-header-left">
+        <img src=${tweet.user.avatars}/>
+        <span>${tweet.user.name}</span>
+      </div>
+      <div class="tweet-header-right" >
+        <span>${tweet.user.handle}</span>
+      </div>
+      </header>
+      <div class="tweet-text">
+        <p>${secureInput(tweet.content.text)}</p>
+      </div>
+      <footer>
+      <div class="tweet-footer-left">
+        <p>${timePassed}</p>
+      </div>
+        <div class="tweet-footer-right">
+          <i id="flag" class="fa-solid fa-flag"></i>
+          <i id="retweet" class="fa-solid fa-retweet"></i>            
+          <i id="heart" class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
+
+  return $tweet;
+};
+
+renderTweets(data);
